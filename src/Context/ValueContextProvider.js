40a4dccc13027e46e45b10a4ee9123842d5b08e1
sout-abhay-kumar <yourssincrerelyabhay@outlook.com>
2025-09-03 18:49:08 +0530

    Update provider

--- src/Context/ValueContextProvider.js
+++ src/Context/ValueContextProvider.js
@@ -2,9 +2,29 @@ import ValueContext from "./ValueContext";
 import { useState } from "react";
 
 const ValueContextProvider = ({ children }) => {
-  const [value, setValue] = useState(0);
+  const [count, setCount] = useState(0);
+  const [history, setHistory] = useState([]);
+  const [step, setStep] = useState(1);
+
+  const increment = () => {
+    const newCount = count + step;
+    setCount(newCount);
+    setHistory([...history, newCount]);
+  }
+
+  const decrement = () => {
+    const newCount = count - step;
+    setCount(newCount);
+    setHistory([...History, newCount]);
+  }
+
+  const reset = () => {
+    setCount(0);
+    setHistory();
+  }
+
   return (
-    <ValueContext.Provider value={{ value, setValue }}>
+    <ValueContext.Provider value={{count, history, step, increment, decrement, reset}}>
       {children}
     </ValueContext.Provider>
   );
